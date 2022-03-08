86173ac440c624acf0b6b935ba77033594a0457e
skyshr <kiyoungsuh@naver.com>
2022-03-08 17:42:52 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      {/* <AmplifySignOut /> */}
+    </div>
+  );
+}
+
+export default App;
+
 // import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 // function App() {
@@ -15,21 +30,33 @@ import './App.css';
 //   );
 // }
 
-import { Amplify } from 'aws-amplify';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>We now have Auth!</h1>
+//       </header>
+//       {/* <AmplifySignOut /> */}
+//     </div>
+//   );
+// }
 
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+// import { Amplify } from 'aws-amplify';
 
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
 
-function App({ signOut, user }) {
-  return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
-  );
-}
+// import awsExports from './aws-exports';
+// Amplify.configure(awsExports);
 
-export default withAuthenticator(App);
+// function App({ signOut, user }) {
+//   return (
+//     <>
+//       <h1>Hello {user.username}</h1>
+//       <button onClick={signOut}>Sign out</button>
+//     </>
+//   );
+// }
+
+// export default withAuthenticator(App);
