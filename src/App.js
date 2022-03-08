cbd2ec08515cc724fe7307ff02410b6d7215e8ee
skyshr <kiyoungsuh@naver.com>
2022-03-08 17:26:37 +0900

    test

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+// import awsExports from './aws-exports';
+// Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
   return (
